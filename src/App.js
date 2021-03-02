17d0bf70d71ea4c61795dcca0c77b42734651c01
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2021-03-02 01:03:35 -0600

    [UPDATE] React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const arrayPost = [
+  {
+    id: 1,
+    title: 'My first Post',
+    text: 'sdnd',
+    date: '2020-09-09'
+  },
+  {
+    id: 2,
+    title: 'My second Post',
+    text: 'sdnd',
+    date: '2020-09-09'
+  },
+  {
+    id: 2,
+    title: 'My third Post',
+    text: 'sdnd',
+    date: '2020-09-09'
+  }
+]
+class App extends Component {
+  render () {
+    return (
+      <div>
+        {
+          arrayPost.map(({ title, text, date, id }) => (
+            <div key={id}>
+              title: {title}
+            </div>
+          ))
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
